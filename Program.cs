dc9dff2a9f8f1ed95c7f265d0442ca43abc855ea
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-16 19:31:58 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HighQualityMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Methods are important- >reduce complexity, improve code readability, 
+            // we avoid duplicating the code, improve the readability
+            // method -on conrete object we call an function-it is method . method is reusable code we can call .//routines, -> procedure
+            // We devide the code into pieces and we increase the readability 
+            // methods simplify the software development--we hide complex things, hide impementation details, 
+            // methods increase a level of abstraction!
+            // method should do what its name says, or should indicate an error when it cant do it then-> trow an exception
+            // its important to throw a correct exception when the methods fails . it must return suitalbe error !
+            // the method does many things not just one - ?  bad Cohesion, strong cohesion-the method does only one thing !
+            //the public mehtods must have high quality, !!! private methods can be not so high-quality code!
+            // Math class is example of strong cohesion--it only does noly mathematical  calculations !!!
+            // loose coupling--we have minimal dependancies, to have, to be independant!
+            // math.sqrt--- ideal coupling, and striong cohesion !
+            // design- pattern ->  strong and difficult logic is hidden into class with some methods. - >facade
+            // reducing coupling --using OOP Principles, abstrracction and Encapsulation
+            // Abstraction --defin a public interface and hide the implementation details.
+            // Encapsuilation -> make methods and fields and properties, private, protected internal;
+            // acceptable coupling, is ---> the method is connetced to ts parameters /
+            // a method in a class is coupled--connected to some private fields!  -this is also acceptable coupling !
+            // when we have a static m ethod, or varialble --it stays in the memory constantly --it takes memory.
+            // limit the numbers of parameters in a metod till 7 max !!
+            // a mehtod should be max 1 screen long -- 30 lines, more importatnt is stron cohesion, and low coupling !
+            // pseudocode is design code with some  algorythm, writen more in words-describes situations.
+            // deprecated method , this method will be deleted, disabled , marked as not supported in near future 
+
+
+
+
+
+
+
+        }
+    }
+}
